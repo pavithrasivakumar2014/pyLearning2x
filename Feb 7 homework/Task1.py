dc019f1e06b4dbd079597b52b56716c0acfd566d
pavithrasivakumar2014 <pavithrasivakumar2014@gmail.com>
2024-02-08 17:12:22 -0400

    My Task

---
+++ Feb 7 homework/Task1.py
@@ -0,0 +1,10 @@
+# 1. Explain the difference between the = operator and the == operator in Python.
+
+# Ans: "=" operator assigns the value from right to left operands.
+# Whereas "==" operator checks the value of right & left operands are equal or not
+
+# 2. What does the ** operator do in Python, and how is it used?
+# Ans: "**" is an Exponential operator. a is raised to the power of b
+
+# 3. What does the ^ operator do in Python, and in what context is it commonly used?
+# Ans: The ^ operator does a binary xor where, a ^ b will return a value with only the bits set in a or in b but not both.
